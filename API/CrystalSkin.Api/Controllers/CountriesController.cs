183dff62f707696fd68143aa36c77a0bee1524ec
Hanaa00 <97117151+Hanaa00@users.noreply.github.com>
2025-07-05 18:32:30 +0200

    Update CountriesController.cs

--- API/CrystalSkin.Api/Controllers/CountriesController.cs
+++ API/CrystalSkin.Api/Controllers/CountriesController.cs
@@ -1,6 +1,6 @@
 ﻿namespace CrystalSkin.Api.Controllers;
 
-//[Authorize(AuthenticationSchemes = "Bearer")]
+[Authorize(AuthenticationSchemes = "Bearer")]
 public class CountriesController : BaseCrudController<CountryModel, CountryUpsertModel, BaseSearchObject, ICountriesService>
 {
     public CountriesController(
